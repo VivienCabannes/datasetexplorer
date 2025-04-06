7e69d3b7b4f4868aa4688d031e8102db109ce861
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-04-07 00:19:21 +0200

    rendering markdown and latex

--- scripts/build.py
+++ scripts/build.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import json
 from flask import Flask, render_template
 
+
 ROOT_DIR = Path(__file__).parent.parent.resolve()
+DATA_DIR = ROOT_DIR / "datasets"
 
 app = Flask(
     __name__,
@@ -13,25 +15,24 @@ app = Flask(
 
 
 def load_datasets():
-    datasets_path = ROOT_DIR / "datasets"
     all_data = []
-    for path in datasets_path.glob("*.json"):
+    for path in DATA_DIR.glob("*.json"):
         with open(path) as f:
             all_data.append(json.load(f))
     return all_data
 
 
 def load_tags():
-    tags_path = ROOT_DIR / "tags.json"
+    tags_path = DATA_DIR / "tags.jsonl"
     with open(tags_path, "r") as f:
         return json.load(f)
 
 
 @app.route("/")
 def index():
-    datasets = load_datasets()
     tags = load_tags()
-    return render_template("index.html", datasets=datasets, tags=tags)
+    datasets = load_datasets()
+    return render_template("index.html", tags=tags, datasets=datasets)
 
 
 if __name__ == "__main__":
